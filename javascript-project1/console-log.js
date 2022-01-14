7bca4bfa5210c54f194a398e715ffe964515a5b2
Atlas205 <78223903+Atlas205@users.noreply.github.com>
2022-01-14 15:40:55 -0600

    Add files via upload

---
+++ javascript-project1/console-log.js
@@ -0,0 +1,34 @@
+"use strict";
+
+console.log(null == undefined); // 
+console.log(typeof (null == undefined));
+
+console.log("NaN" == NaN);
+console.log(typeof ("NaN" == NaN));
+
+console.log(5 == NaN);
+console.log(typeof (5 == NaN));
+
+console.log(NaN == NaN);
+console.log(typeof (NaN == NaN));
+
+console.log(NaN != NaN);
+console.log(typeof (NaN !== NaN));
+
+console.log(false == 0);
+console.log(typeof (false == 0));
+
+console.log(true == 1);
+console.log(typeof (true == 1));
+
+console.log(true === 1);
+console.log(typeof (true === 1));
+
+console.log(true == 2);
+console.log(typeof (true == 2));
+
+console.log(undefined == 0);
+console.log(typeof (undefined == 0));
+
+console.log(null == 0);
+console.log(typeof (null == 0));
